90520be458f136130c74bc11f92fe0014eac4e50
ekrem-aktas <ekrem-aktas@hotmail.com>
2017-06-02 15:20:01 +0200

    fix endpoint

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.get("/", function(req, res) {
+app.get("/test", function(req, res) {
     res.send("okay");
 });
 
