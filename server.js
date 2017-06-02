4b0677159f550ad4ba4ea9784da03a9ea1358e66
ekrem-aktas <ekrem-aktas@hotmail.com>
2017-06-02 14:56:14 +0200

    fix ip address

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get("/", function(req, res) {
     res.send("okay");
