e3fe65bc2fe4b956609233cdbaa5d4f55a97e6c1
ekrem-aktas <ekrem-aktas@hotmail.com>
2017-06-02 14:34:13 +0200

    add express properly

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 app.get("/", function(req, res) {
     res.send("okay");
 });
 
-app.listen(80, () => {
+app.listen(port, ip, () => {
     console.log("App started listening");
 });
