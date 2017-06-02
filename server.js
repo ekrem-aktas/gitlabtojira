b42e49ab9d292881941309b7dfbbab1c9b77b143
ekrem-aktas <ekrem-aktas@hotmail.com>
2017-06-02 16:02:23 +0200

    change entry

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 80;
 const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.get("/test", function(req, res) {
+app.get("/", function(req, res) {
     res.send("okay");
 });
 
