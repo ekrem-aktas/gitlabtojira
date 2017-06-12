abb4910c797c8ec4db3f3837ec5401eb8ca99555
ekrem-aktas <ekrem-aktas@hotmail.com>
2017-06-12 09:09:30 +0200

    Finally

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 80;
-const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const port = process.env.PORT || 80;
+const ip   = /*process.env.IP ||*/ '0.0.0.0';
 
 app.get("/", function(req, res) {
     res.send("okay");
@@ -11,5 +11,5 @@ app.get("/", function(req, res) {
 console.log("Will listen on port " + port + " and ip " + ip);
 
 app.listen(port, ip, () => {
-    console.log("App started listening on port " + port + " and ip " + ip);
+    console.log("aApp started listening on port " + port + " and ip " + ip);
 });
