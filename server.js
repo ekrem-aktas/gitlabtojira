65e906a2d9a10ac10be9d4f9d0568a4b85365347
ekrem-aktas <ekrem-aktas@hotmail.com>
2017-06-12 09:32:05 +0200

    Add body parser

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 80;
 const ip   = /*process.env.IP ||*/ '0.0.0.0';
 
+app.use(bodyParser.json());
+
 app.get("/", function(req, res) {
-    res.send("okay");
+    res.send("okay!!");
+});
+
+app.post("/", function(req, res) {
+    const mergeRequest = req.body;
+    res.json(mergeRequest);
 });
 
 console.log("Will listen on port " + port + " and ip " + ip);
